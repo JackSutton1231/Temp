f5f57e1bd7ecf27ee5a5669b2cfb37e1014075ce
JackSutton1231 <2024sutton.jack@fcboe.org>
2025-01-08 13:33:41 -0500

    Update temp_disp.py
    Done

--- temp_disp.py
+++ temp_disp.py
@@ -1,17 +1,100 @@
+# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
 
+"""
+This test will initialize the display using displayio and draw a solid green
+background, a smaller purple rectangle, and some yellow text.
+"""
+import board
+import terminalio
+import displayio
+from analogio import AnalogIn
+import pwmio
 import time
 import gc
-import board
-import displayio
 import random
-from adafruit_st7789 import ST7789
 from adafruit_display_shapes.circle import Circle
 from adafruit_display_shapes.rect import Rect
 from adafruit_display_shapes.polygon import Polygon
-from analogio import AnalogIn
-import pwmio
+
+# Starting in CircuitPython 9.x fourwire will be a seperate internal library
+# rather than a component of the displayio library
+try:
+    from fourwire import FourWire
+except ImportError:
+    from displayio import FourWire
+from adafruit_display_text import label
+from adafruit_st7789 import ST7789
+
 
 pot = AnalogIn(board.A0)
+
+
+maximum = 65535
+volts = pot.value / maximum * 3.3
+tempC = (volts - .5) * 100
+tempF = (9/5) * tempC + 32
+tempF = int(tempF * 100) / 100
+tempF_str = str(tempF)
+
+
+
+
+
+# First set some parameters used for shapes and text
+BORDER = 20
+FONTSCALE = 2
+BACKGROUND_COLOR = 0x00FF00  # Bright Green
+FOREGROUND_COLOR = 0xAA0088  # Purple
+TEXT_COLOR = 0xFFFF00
+
+# Release any resources currently in use for the displays
+displayio.release_displays()
+
+spi = board.SPI()
+tft_cs = board.D2
+tft_dc = board.D3
+
+display_bus = FourWire(spi, command=tft_dc, chip_select=tft_cs)
+display = ST7789(
+    display_bus, rotation=270, width=240, height=135, rowstart=40, colstart=53
+)
+
+# Make the display context
+splash = displayio.Group()
+display.root_group = splash
+
+color_bitmap = displayio.Bitmap(display.width, display.height, 1)
+color_palette = displayio.Palette(1)
+color_palette[0] = BACKGROUND_COLOR
+
+bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
+splash.append(bg_sprite)
+
+# Draw a smaller inner rectangle
+inner_bitmap = displayio.Bitmap(
+    display.width - BORDER * 2, display.height - BORDER * 2, 1
+)
+inner_palette = displayio.Palette(1)
+inner_palette[0] = FOREGROUND_COLOR
+inner_sprite = displayio.TileGrid(
+    inner_bitmap, pixel_shader=inner_palette, x=BORDER, y=BORDER
+)
+splash.append(inner_sprite)
+
+# Draw a label
+text = tempF_str
+text_area = label.Label(terminalio.FONT, text=text, color=TEXT_COLOR)
+text_width = text_area.bounding_box[2] * FONTSCALE
+text_group = displayio.Group(
+    scale=FONTSCALE,
+    x=display.width // 2 - text_width // 2,
+    y=display.height // 2,
+)
+text_group.append(text_area)  # Subgroup for text scaling
+splash.append(text_group)
+
+
 
 
 while True:
@@ -19,7 +102,9 @@ while True:
     volts = pot.value / maximum * 3.3 
     tempC = (volts - .5) * 100
     tempF = (9/5) * tempC + 32
+    tempF = int(tempF * 100) / 100
     tempF_str = str(tempF)
     
-    print(tempF_str)
-    time.sleep(.5)
+    text_area.text = tempF_str + " Deg F" 
+    time.sleep(5)
+
